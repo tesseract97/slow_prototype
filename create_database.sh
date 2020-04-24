67273170a0afe5bd9a9c55a11fd9d08d728e7eee
tesseract97 <47134315+tesseract97@users.noreply.github.com>
2020-04-24 12:10:04 -0400

    Edit

--- create_database.sh
+++ create_database.sh
@@ -9,7 +9,7 @@ db_name=$1
 #curl command sends http request with admin name and password to local address with CouchDB port
 #the desired database name is added to the end of the command
 COMMAND="docker exec slow_control curl -X PUT http://EXO:x3n0ntpc@127.0.0.1:5984/"
-COMMAND2="${COMMAND}/${db_name}"
+COMMAND2="${COMMAND}${db_name}"
 
 #response evaluates the command
 RESPONSE=`$COMMAND2`
