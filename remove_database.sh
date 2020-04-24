3875cc57cf43e5bb320081102d8d2563bc2c1ff6
tesseract97 <47134315+tesseract97@users.noreply.github.com>
2020-04-24 12:18:58 -0400

    Upload commented remove_database file

---
+++ remove_database.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#first parameter is the desired database name
+
+db_name=$1
+
+#docker executes the curl command on the slow_control container with the admin name and password
+#curl command sends http request with admin name and password to local address with CouchDB port
+#the desired database name is added to the end of the command
+COMMAND="docker exec slow_control curl -X DELETE http://EXO:x3n0ntpc@127.0.0.1:5984/"
+COMMAND2="${COMMAND}${db_name}"
+
+#response evaluates the command
+RESPONSE=`$COMMAND2`
+
+#the result is printed for confirmation of database removal
+echo $RESPONSE
+
+
+exit 0
